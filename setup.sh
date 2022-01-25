31ff84cc3261141d41d97895fce35ec452b4a21b
mrrbrilliant <mrrbrilliantwork@gmail.com>
2022-01-26 05:40:46 +0700

    update

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ function prepare_env() {
 
     echo "Enter your database port"
     read MONGO_PORT
-    sed -i "s/27010/${MONGO_PORT}/g" $SERVER_DIR/docker-compose.yml
+    sed -i "s/27010/$MONGO_PORT/g" $SERVER_DIR/docker-compose.yml
 
     {
         echo -e "# For MongoDB"
@@ -51,8 +51,8 @@ function prepare_env() {
     } > $SERVER_DIR/.env
 
     cd $SERVER_DIR
-    sed -i -e "s/your_path/${SERVER_DIR}/g" db.service
-    sed -i -e "s/your_path/${SERVER_DIR}/g" server.service
+    sed -i "s/your_path/$SERVER_DIR/g" db.service
+    sed -i "s/your_path/$SERVER_DIR/g" server.service
 
     sudo install -Dm644 db.service /etc/systemd/system/onelab-db.service
     sudo install -Dm644 server.service /etc/systemd/system/onelab-server.service
