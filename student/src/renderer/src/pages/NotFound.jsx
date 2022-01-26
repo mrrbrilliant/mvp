23ef39a5084899c8e2432e533f898a8cad66bb31
mrrbrilliant <mrrbrilliantwork@gmail.com>
2022-01-26 09:12:32 +0700

    update

--- student/src/renderer/src/pages/NotFound.jsx
+++ student/src/renderer/src/pages/NotFound.jsx
@@ -7,13 +7,13 @@ export default function NotFound() {
 	return (
 		<div className="w-screen h-screen text-yellow-600 select-none cursor-not-allowed bg-slate-900 grid place-items-center">
 			<div className="grid place-items-center gap-8">
-				<h1 className="text-6xl font-bold">៤០៤</h1>
-				<p className="text-2xl font-light">មិនមានទំព័រមិនត្រឹមត្រូវ</p>
+				<h1 className="text-6xl font-bold">ស្វាគមន៍</h1>
+				{/* <p className="text-2xl font-light">មិនមានទំព័រមិនត្រឹមត្រូវ</p> */}
 				<button
 					onClick={() => navigate("/")}
 					className="px-4 py-2 bg-none border-2 border-yellow-600 rounded-lg "
 				>
-					ទៅទំព័រដើម
+					តោះចូលរៀន
 				</button>
 			</div>
 		</div>
